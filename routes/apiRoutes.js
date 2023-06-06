de558853cf2463f8e3bb1404d7eff2328001b643
Alcantn1 <nalcantara92@gmail.com>
2023-06-06 14:55:28 -0700

    serverjs and html.js added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,21 @@
+const express = require('express');
 const router = require('express').Router();
 const store = require('../db/store');
 
+router.get('/api/notes', (req, res) => {
+    store.getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+});
+  router.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    store.addNote(newNote)
+      .then((note) => {
+        return res.json(note);
+      })
+    });
 
+  
 module.exports = router;
 
