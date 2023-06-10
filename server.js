f9fd0aec13d0f6d1bf89229cdb8641b072068b25
Alcantn1 <nalcantara92@gmail.com>
2023-06-09 20:39:42 -0700

    added code

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
